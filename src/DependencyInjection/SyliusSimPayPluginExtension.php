a73e8ec5cd504f13cd287579b47ea6327dfe3614
Dominik Płachta <dominik.plachta@8lines.io>
2023-04-12 19:34:12 +0200

    Fix problem with plugin loading

--- src/DependencyInjection/SyliusSimPayPluginExtension.php
+++ src/DependencyInjection/SyliusSimPayPluginExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class BitBagSyliusBlacklistExtension extends Extension
+final class SyliusSimPayPluginExtension extends Extension
 {
     public function load(array $config, ContainerBuilder $container): void
     {
