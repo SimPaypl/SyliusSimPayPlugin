b44e0beee00bfd3478e71b914c6c0ba6680c8d42
Dominik Płachta <dominik.plachta@8lines.io>
2023-04-14 01:52:57 +0200

    Add SimPay's Gateway configuration form

---
+++ src/Form/Type/SimPayGatewayConfigurationType.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EightLines\SyliusSimPayPlugin\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+final class SimPayGatewayConfigurationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add(
+                'simpay_api_key',
+                TextType::class,
+                [
+                    'label' => 'eightlines.sylius_simpay_plugin.api_key',
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'eightlines.sylius_simpay_plugin.gateway_configuration.api_key.not_blank',
+                                'groups' => ['sylius'],
+                            ]
+                        ),
+                    ],
+                ]
+            )
+            ->add(
+                'simpay_api_password',
+                TextType::class,
+                [
+                    'label' => 'eightlines.sylius_simpay_plugin.api_password',
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'eightlines.sylius_simpay_plugin.gateway_configuration.api_password.not_blank',
+                                'groups' => ['sylius'],
+                            ]
+                        ),
+                    ],
+                ]
+            )
+            ->add(
+                'simpay_service_id',
+                TextType::class,
+                [
+                    'label' => 'eightlines.sylius_simpay_plugin.service_id',
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'eightlines.sylius_simpay_plugin.gateway_configuration.service_id.not_blank',
+                                'groups' => ['sylius'],
+                            ]
+                        ),
+                    ],
+                ]
+            )
+            ->add(
+                'simpay_service_api_key',
+                TextType::class,
+                [
+                    'label' => 'eightlines.sylius_simpay_plugin.service_api_key',
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'eightlines.sylius_simpay_plugin.gateway_configuration.service_api_key.not_blank',
+                                'groups' => ['sylius'],
+                            ]
+                        ),
+                    ],
+                ]
+            )
+        ;
+    }
+}
