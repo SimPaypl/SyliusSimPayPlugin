3321cfb7b150694dcd665be2a159f67ecbe8d15c
Dominik Płachta <dominik.plachta@8lines.io>
2023-04-20 16:12:09 +0200

    Remove @throws Exception

--- src/DependencyInjection/Compiler/OverridePaymentMethodsResolverPass.php
+++ src/DependencyInjection/Compiler/OverridePaymentMethodsResolverPass.php
@@ -7,14 +7,10 @@ namespace SimPay\SyliusSimPayPlugin\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use SimPay\SyliusSimPayPlugin\Resolver\PaymentMethodsResolver;
-use Exception;
 use Symfony\Component\DependencyInjection\Reference;
 
 final class OverridePaymentMethodsResolverPass implements CompilerPassInterface
 {
-    /**
-     * @throws Exception
-     */
     public function process(ContainerBuilder $container): void
     {
         $container->getDefinition('sylius.payment_methods_resolver')
